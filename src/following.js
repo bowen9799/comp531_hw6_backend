66b757aaabad942f7d375b60c1191d1cd07e39cf
bowen9799 <bowen9799@gmail.com>
2017-04-14 14:40:55 -0500

    backend

---
+++ src/following.js
@@ -0,0 +1,35 @@
+// Handles all following-related requests
+
+// Constants
+const username = 'bl19'
+
+// Handlers
+// GET handler -> /following
+const getFollowing = (req, res) => {
+	return res.send({
+		username,
+		following: []
+	})
+}
+
+// PUT handler -> /following
+const putFollowing = (req, res) => {
+	return res.send({
+		username,
+		following: []
+	})
+}
+
+// DELETE handler -> /following
+const deleteFollowing = (req, res) => {
+	return res.send({
+		username,
+		following: []
+	})
+}
+
+module.exports = app => {
+     app.get('/following/:user?', getFollowing)
+     app.put('/following/:user', putFollowing)
+     app.delete('/following/:user', deleteFollowing)
+}
